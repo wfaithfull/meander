716d8c36fdfb7b66d190c6dba6e6896e432500e3
Will Faithfull <will@sourcepulp.com>
2017-12-05 16:03:45 +0000

    Comment the spliterator

--- meander-core/src/main/java/uk/ac/bangor/meander/streams/ExampleSpliterator.java
+++ meander-core/src/main/java/uk/ac/bangor/meander/streams/ExampleSpliterator.java
@@ -7,17 +7,28 @@ import java.util.function.Consumer;
 
 /**
  * @author Will Faithfull
+ *
+ * Spliterator of {@link Example} that can be used to stream an {@link ExampleProviderFactory} and advance a
+ * {@link StreamContext}.
  */
 class ExampleSpliterator implements Spliterator<Example> {
 
     private ExampleProviderFactory source;
     private StreamContext          context;
 
+    /**
+     * Create the spliterator.
+     * @param source Factory for {@link ExampleProvider}s.
+     * @param context Stream context component.
+     */
     ExampleSpliterator(@NonNull final ExampleProviderFactory source, @NonNull final StreamContext context) {
         this.source = source;
         this.context = context;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean tryAdvance(Consumer<? super Example> action) {
         action.accept(source.getProvider().apply(context));
@@ -25,16 +36,25 @@ class ExampleSpliterator implements Spliterator<Example> {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Spliterator<Example> trySplit() {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public long estimateSize() {
         return -1;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int characteristics() {
         return ORDERED | NONNULL | IMMUTABLE;
