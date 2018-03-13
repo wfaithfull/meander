eff96bcc25bfb493bc44846558f43bacf87766eb
Will Faithfull <will@sourcepulp.com>
2018-03-13 22:58:22 +0000

    add windowpairpipe

---
+++ meander-detectors/src/main/java/uk/ac/bangor/meander/detectors/windowing/WindowPairPipe.java
@@ -0,0 +1,37 @@
+package uk.ac.bangor.meander.detectors.windowing;
+
+import uk.ac.bangor.meander.detectors.Pipe;
+import uk.ac.bangor.meander.streams.StreamContext;
+
+/**
+ * @author Will Faithfull
+ */
+public class WindowPairPipe implements Pipe<Double[], WindowPair<Double[]>> {
+
+    private WindowPair<Double[]> windowPair;
+
+    public WindowPairPipe(WindowPair<Double[]> windowPair) {
+        this.windowPair = windowPair;
+    }
+
+    public WindowPairPipe(int size) {
+        this.windowPair = new WindowPair<>(size, size, Double[].class);
+    }
+
+
+    @Override
+    public WindowPair<Double[]> execute(Double[] value, StreamContext context) {
+        windowPair.update(value);
+        return windowPair;
+    }
+
+    @Override
+    public boolean ready() {
+        return windowPair.size() == windowPair.capacity();
+    }
+
+    @Override
+    public void reset() {
+        windowPair.clear();
+    }
+}
