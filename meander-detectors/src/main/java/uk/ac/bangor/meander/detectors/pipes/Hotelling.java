8feecc8fc8472826d3a2ee530fda52b521115511
Will Faithfull <will@sourcepulp.com>
2018-03-14 18:40:22 +0000

    Remove unused fdistribution from TsqReduction

--- meander-detectors/src/main/java/uk/ac/bangor/meander/detectors/pipes/Hotelling.java
+++ meander-detectors/src/main/java/uk/ac/bangor/meander/detectors/pipes/Hotelling.java
@@ -1,6 +1,5 @@
 package uk.ac.bangor.meander.detectors.pipes;
 
-import org.apache.commons.math3.distribution.FDistribution;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.SingularValueDecomposition;
@@ -18,8 +17,6 @@ public class Hotelling {
     public static class TsqReduction implements Pipe<WindowPair<Double[]>, CDF.FStatisticAndDegreesFreedom> {
 
         private static final int MAX_CONDITION = 10000;
-        private FDistribution        fDistribution;
-
         private int df1,df2;
 
         private double[] getDiagonal(RealMatrix matrix) {
@@ -32,19 +29,6 @@ public class Hotelling {
                 diagonal[col] = data[i][col++];
             }
             return diagonal;
-        }
-
-        public boolean decide(Double tsq) {
-
-            if(df1 <= 0 || df2 <= 0) {
-                return false;
-            }
-
-            fDistribution = new FDistribution(df1, df2);
-
-            double pst = 1-fDistribution.cumulativeProbability(tsq);
-
-            return pst < 0.05;
         }
 
         @Override
