101d206697a331f4c929c82e753992dcf5ad5a08
Will Faithfull <will@sourcepulp.com>
2018-05-29 13:54:55 +0100

    Add fixed tail window pair

---
+++ meander-detectors/src/main/java/uk/ac/bangor/meander/detectors/windowing/FixedTailWindowPair.java
@@ -0,0 +1,23 @@
+package uk.ac.bangor.meander.detectors.windowing;
+
+/**
+ * @author Will Faithfull
+ */
+public class FixedTailWindowPair<T> extends WindowPair<T> {
+    public FixedTailWindowPair(int w1sz, int w2sz, Class<T> tClass) {
+        super(w1sz, w2sz, tClass);
+    }
+
+    public FixedTailWindowPair(Window<T> tail, Window<T> head) {
+        super(tail, head);
+    }
+
+    @Override
+    public void update(T observation) {
+        if (head.size() == head.capacity() && tail.size() != tail.capacity()) {
+            tail.update(head.getOldest());
+        }
+
+        head.update(observation);
+    }
+}
